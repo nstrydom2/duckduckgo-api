52e6425e0a042f8ac910ccd2c5ae87320fe5749e
Nick <nstrydom@gmail.com>
2020-04-25 06:00:31 -0500

    Implement Result.java

---
+++ src/main/java/org/bitnick/duckduckgo/web/search/Result.java
@@ -0,0 +1,31 @@
+package org.bitnick.duckduckgo.web.search;
+
+public class Result {
+    private String title;
+    private String url;
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
